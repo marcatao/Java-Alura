343dade12aa61427a2e2f4c1f310d1a2bdba6a4a
marcatao <thiagomarcato@gmail.com>
2020-07-19 17:53:49 -0300

    conhecendo Arrays

---
+++ bytebank/src/ContaCorrente.java
@@ -0,0 +1,16 @@
+
+public class ContaCorrente {
+
+	private int agencia, conta;
+
+	
+	public ContaCorrente(int agencia,int conta) {
+		this.agencia = agencia;
+		this.conta = conta;
+		
+	}
+	
+	public int getNumero() {
+		return this.conta;
+	}
+}
